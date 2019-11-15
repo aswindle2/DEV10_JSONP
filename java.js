8ed28dc37449296e77e9ff597d487e43c93a8cac
aswindle2 <54967533+aswindle2@users.noreply.github.com>
2019-11-14 22:28:29 -0600

    Add files via upload

--- java.js
+++ java.js
@@ -14,25 +14,6 @@ function showEvents(data){
   
 }
 
-showEvents({
-	"events":[
-	{
-		"location": "San Francisco, CA",
-		"date": "May 1",
-		"map": "http://javascriptbook.com/code/c08/img/map-ca.png"
-	}
-	{
-		"location": "Austin,TX",
-		"date": "May 15",
-		"map": "http://javascriptbook.com/code/c08/img/map-tx.png"
-	}
-	{
-		"location": "New York, NY",
-		"date": "May 30",
-		"map: "http://javascriptbook.com/code/c08/img/map-ny.png"
-	}
-	]
-});
 
 
 // When working locally in Firefox, you may see an error saying that the JSON is not well-formed.
